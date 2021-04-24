f4afcfcb8db2db5c53ad0c902f10899f702e77fe
Amrut-47 <gpkp156027@gmail.com>
2021-04-24 11:58:05 +0530

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express();
-
+const port  = process.env.PORT || 5000;
 
 // Define paths for express config
 const PublicDirectoryPath = path.join(__dirname, '../public')
@@ -105,6 +105,6 @@ app.get('*',(req, res)=>
     })
 })
 
-app.listen('5000',()=>{
-    console.log("listening from localhost...!")
+app.listen(port,()=>{
+    console.log("listening from port "+port)
 })
